e21aebb4c730deacb1e6b5581a5969e96efa12e6
Sherdil-Asif <72036933+Sherdil-Asif@users.noreply.github.com>
2020-10-20 08:23:03 -0700

    While loop Program Added

---
+++ While.java
@@ -0,0 +1,10 @@
+class WhileLoop {
+    public static void main(String args[]){
+         int n=5;
+         while(n<=10){
+              System.out.println("7");
+              System.out.println("Sherdil");
+              n++;
+         }
+    }
+}
